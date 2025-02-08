5cf28739beb28ae1165ee2999b9a8c80af6414f2
ZhangJinchao7 <blue.zhang@thecarevoice.com>
2025-02-08 16:22:27 +0800

    add maven username and password 1. update token

--- WellnessDemo/settings.gradle.kts
+++ WellnessDemo/settings.gradle.kts
@@ -22,8 +22,8 @@ dependencyResolutionManagement {
             setUrl("https://nexus.kangyu.info/repository/maven-snapshots/")
             //上传服务器的账户密码
             credentials {
-                username = "Your username"
-                password = "Your password"
+                username = "cvpartner"
+                password = "wVSmP7GU8z"
             }
         }
     }
